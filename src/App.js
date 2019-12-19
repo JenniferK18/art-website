ec6bf00ae6a7c191da4fd28943d8c05450ad1a73
unknown <j.krause2018@gmail.com>
2019-12-19 11:33:12 -0700

    feat(banner): banner photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import banner from './assets/banner-photo.jpeg';
 
 function App() {
   return (
-    <NavBar />
+    <Fragment>
+      <img 
+        src={banner}
+        style={{ width: '100%', height: 300, objectFit: 'cover' }}
+        alt=""
+        className="img-fluid"
+      />
+      <NavBar />
+    </Fragment>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import '../css/index.css'
 import {
