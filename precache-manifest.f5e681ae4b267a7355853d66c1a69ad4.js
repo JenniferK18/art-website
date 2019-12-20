b659edfef32068e17a9f71c6e7ea995bd2af0d09
unknown <j.krause2018@gmail.com>
2019-12-19 20:14:44 -0700

    Updates

---
+++ precache-manifest.f5e681ae4b267a7355853d66c1a69ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c45c01db219d781380f3afc19ceadd",
+    "url": "/art-website/index.html"
+  },
+  {
+    "revision": "677286093aa79c3c36cb",
+    "url": "/art-website/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "677286093aa79c3c36cb",
+    "url": "/art-website/static/js/2.af43de0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/art-website/static/js/2.af43de0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe1e7658bf374e0986cd",
+    "url": "/art-website/static/js/main.e7cbbc2e.chunk.js"
+  },
+  {
+    "revision": "7e173986dc5897ee67f1",
+    "url": "/art-website/static/js/runtime-main.70f1053e.js"
+  },
+  {
+    "revision": "4c96d3a87d8b7512ae3fd65de42a7e4c",
+    "url": "/art-website/static/media/banner-photo.4c96d3a8.jpeg"
+  }
+]);
