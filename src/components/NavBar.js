42a0d5579f5973f3f4378a39ad9b178a05914ad7
unknown <j.krause2018@gmail.com>
2019-12-19 21:21:43 -0700

    fix(route): routing issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,10 +17,10 @@ function NavBar() {
   return (
     <Router>
         <header className="navbar navbar-expand bg-dark border-top border-bottom">
-            <Link to="/"><span className="bg-dark text-white nav-link">Home</span></Link>
-            <Link to="/gallery"><span className="bg-dark text-white nav-link">Gallery</span></Link>
-            <Link to="/pricing"><span className="bg-dark text-white nav-link">Pricing</span></Link>
-            <Link to="/about"><span className="bg-dark text-white nav-link">About me</span></Link>
+            <Link to="/stephanie/"><span className="bg-dark text-white nav-link">Home</span></Link>
+            <Link to="/stephanie/gallery"><span className="bg-dark text-white nav-link">Gallery</span></Link>
+            <Link to="/stephanie/pricing"><span className="bg-dark text-white nav-link">Pricing</span></Link>
+            <Link to="/stephanie/about"><span className="bg-dark text-white nav-link">About me</span></Link>
         </header>
         <Switch>
           <Route exact path="/stephanie/">
